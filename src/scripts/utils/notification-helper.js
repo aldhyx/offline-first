40fb43a504db5517c5efc2cefb9fd12be080cb4e
fsaythanry <aldyzkobf@gmail.com>
2023-09-27 16:32:17 +0800

    notification using web socket

---
+++ src/scripts/utils/notification-helper.js
@@ -0,0 +1,46 @@
+const NotificationHelper = {
+  sendNotification({ title, options }) {
+    if (!this._checkAvailability()) {
+      console.log('Notification not supported in this browser');
+      return;
+    }
+    if (!this._checkPermission()) {
+      console.log('User did not yet granted permission');
+      this._requestPermission();
+      return;
+    }
+
+    this._showNotification({ title, options });
+  },
+
+  _checkAvailability() {
+    return 'Notification' in window;
+  },
+
+  _checkPermission() {
+    return Notification.permission === 'granted';
+  },
+
+  async _requestPermission() {
+    const status = await Notification.requestPermission();
+
+    if (status === 'denied') {
+      console.log('Notification Denied');
+      return;
+    }
+
+    if (status === 'default') {
+      console.log('Permission Closed');
+      return;
+    }
+
+    console.log('Notification Permission Granted');
+  },
+
+  async _showNotification({ title, options }) {
+    const serviceWorkerRegistration = await navigator.serviceWorker.ready;
+    serviceWorkerRegistration.showNotification(title, options);
+  },
+};
+
+export default NotificationHelper;
